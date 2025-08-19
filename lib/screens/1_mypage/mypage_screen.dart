08000dbab5b370c124291f6105f43c203f6a65ac
June196 <1126jypark@snu.ac.kr>
2025-08-19 20:35:58 +0900

    250819/지윤/시원님 작업 내용 중 mypage와 joingroup만 반영

--- lib/screens/1_mypage/mypage_screen.dart
+++ lib/screens/1_mypage/mypage_screen.dart
@@ -56,6 +56,7 @@ class _MyPageScreenState extends State<MyPageScreen> {
           'groupName': data['groupName'],
           'invitationCode': data['invitationCode'],
           'imageUrl': data['imageUrl'] ?? '',
+          'membersCount': data['membersCount'] ?? 0,
         };
       }),
     );
@@ -225,6 +226,14 @@ class _MyPageScreenState extends State<MyPageScreen> {
                                           const SizedBox(height: 7),
                                           Text(
                                             '팀 코드: ${group['invitationCode']}',
+                                            style: const TextStyle(
+                                              color: Colors.white70,
+                                              fontSize: 15,
+                                            ),
+                                          ),
+                                          const SizedBox(height: 4),
+                                          Text(
+                                            '구성원: ${group['membersCount']}명',
                                             style: const TextStyle(
                                               color: Colors.white70,
                                               fontSize: 15,
